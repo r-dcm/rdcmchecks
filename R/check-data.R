7eb939a0fa5f5d7335d66720c34e9711c66f08aa
Jake Thompson <wjakethompson@gmail.com>
2025-01-28 14:30:35 -0600

    add functions for checking data

---
+++ R/check-data.R
@@ -0,0 +1,229 @@
+#' Check that data follows the expected structure
+#'
+#' The data should be 1 row per respondent and 1 column per item, with an
+#' optional additional column to store respondent identifiers. Each value of the
+#' data should be a 0 or 1 to indicate the response to the item by the given
+#' respondent. [clean_data()] calls [check_data()] to verify the expected
+#' structure, and then performs additional data manipulation to provide standard
+#' conventions. See details for additional information.
+#'
+#' @inheritParams abort_bad_argument
+#' @param x The provided data to check.
+#' @param identifier The provided respondent identifier, as a character string.
+#'   If no respondent identifier is present, the value should be `NULL` (the
+#'   default).
+#' @param missing A expression specifying how missing data in `x` is coded
+#'   (e.g., `NA`, `"."`, `-99`). The default is `NA`.
+#'
+#' @details
+#' In many instances, it's important to have standard conventions for a data
+#' object so that we know what to expect (e.g., respondent and item identifiers,
+#' data types). [clean_data()] provides this standardization. Cleaned data is
+#' returned in long format, with one row per response. Respondent and item
+#' columns are encoded as factors, and responses are coerced to integer values.
+#'
+#' To ensure downstream functions are able to identify the original
+#' (pre-cleaned) values, [clean_data()] returns a list that includes the cleaned
+#' data, as well as metadata that includes look-ups from the original to cleaned
+#' values.
+#'
+#'
+#' @returns `check_data` returns the original data (if the checks pass) as a
+#'   [tibble][tibble::tibble-package], with missing data (i.e., `missing`)
+#'   replaced with `NA`.
+#' @export
+#'
+#' @examples
+#' example_data <- tibble::tibble(person = 1:10,
+#'                                item1 = sample(0:1, 10, replace = TRUE),
+#'                                item2 = sample(0:1, 10, replace = TRUE),
+#'                                item3 = sample(0:1, 10, replace = TRUE))
+#' check_data(example_data, identifier = "person")
+check_data <- function(x, identifier = NULL, missing = NA,
+                       arg = rlang::caller_arg(x),
+                       call = rlang::caller_env()) {
+  if (!is.null(identifier) && !(identifier %in% colnames(x))) {
+    abort_bad_argument(arg = arg,
+                       custom = cli::format_message(
+                         c("Specified {.arg identifier}, {.val {identifier}},",
+                           "not found in {.arg {arg}}")
+                       ),
+                       call = call)
+  } else if (!is.null(identifier)) {
+    identifier <- rlang::enquo(identifier)
+  }
+
+  if (!inherits(x, "data.frame")) {
+    abort_bad_argument(arg = arg, must = "be a data frame", not = typeof(x),
+                       call = call)
+  }
+
+  # replace missing values with NA
+  x <- x |>
+    dplyr::mutate(
+      dplyr::across(
+        dplyr::where(~ typeof(.x) == typeof(missing)) & -{{ identifier }},
+        \(x) dplyr::na_if(x, missing)
+      )
+    ) |>
+    dplyr::mutate(dplyr::across(dplyr::where(\(x) is.character(x)),
+                                \(x) readr::parse_guess(x)))
+
+  if (!all(sapply(dplyr::select(x, -{{ identifier }}), is.numeric))) {
+    abort_bad_argument(arg = arg,
+                       must = paste0("contain only numeric values of 0 or 1 ",
+                                     "in response columns"),
+                       call = call)
+  }
+  x <- dplyr::mutate(x, dplyr::across(-{{ identifier }}, as.integer))
+
+  if (!all(sapply(dplyr::select(x, -{{ identifier }}),
+                  function(.x) all(.x %in% c(0L, 1L))))) {
+    abort_bad_argument(arg = arg,
+                       must = "contain only 0 or 1 in response columns",
+                       call = call)
+  }
+
+  if (!tibble::is_tibble(x)) {
+    tibble::as_tibble(x)
+  } else {
+    x
+  }
+}
+
+#' @param cleaned_qmatrix A cleaned Q-matrix, from [clean_qmatrix()].
+#' @param arg_qmatrix A character string with the name of the argument used to
+#'   provide the Q-matrix.
+#'
+#' @returns `clean_data` returns a list with five elements:
+#'  * `clean_data`: The cleaned data
+#'  * `item_identifier`: The real name of the item identifier
+#'  * `item_names`: The real names of the items
+#'  * `respondent_identifier`: The real name of the respondent identifier
+#'  * `respondent_names`: The real names of the respondents
+#'
+#' @export
+#' @rdname check_data
+#' @examples
+#' example_qmatrix <- tibble::tibble(item = paste0("item", 1:3),
+#'                                   att_1 = c(0, 0, 1),
+#'                                   att_2 = c(1, 1, 1))
+#'
+#' example_data <- tibble::tibble(person = 1:10,
+#'                                item1 = sample(0:1, 10, replace = TRUE),
+#'                                item2 = sample(0:1, 10, replace = TRUE),
+#'                                item3 = sample(0:1, 10, replace = TRUE))
+#'
+#' qmatrix <- clean_qmatrix(example_qmatrix, identifier = "item")
+#' clean_data(example_data, identifier = "person",
+#'            cleaned_qmatrix = qmatrix)
+clean_data <- function(x, identifier = NULL, missing = NA, cleaned_qmatrix,
+                       arg_qmatrix = rlang::caller_arg(cleaned_qmatrix),
+                       arg = rlang::caller_arg(x),
+                       call = rlang::caller_env()) {
+  arg <- arg
+  x <- check_data(x, identifier = identifier, missing = missing,
+                  arg = arg, call = call)
+
+  if (is.null(identifier)) {
+    x <- tibble::rowid_to_column(x, var = "resp_id")
+    identifier <- "resp_id"
+  }
+
+  data_names <- rlang::set_names(seq_len(ncol(x) - 1),
+                                 colnames(x)[-which(colnames(x) == identifier)])
+  meta <- c(
+    reconcile_item_names(data_names = data_names,
+                         qmat_names = cleaned_qmatrix$item_names,
+                         qmat_id = cleaned_qmatrix$item_identifier,
+                         arg_data = arg,
+                         arg_qmat = arg_qmatrix,
+                         call = call),
+    list(
+      respondent_identifier = identifier,
+      respondent_names = rlang::set_names(seq_len(nrow(x)), x[[identifier]])
+    )
+  )
+
+  clean_data <- x |>
+    tidyr::pivot_longer(cols = -{{ identifier }}, names_to = "item_id",
+                        values_to = "score") |>
+    dplyr::filter(!is.na(.data$score)) |>
+    dplyr::rename(resp_id = {{ identifier }}) |>
+    dplyr::mutate(
+      resp_id = factor(.data$resp_id, levels = names(meta$respondent_names)),
+      item_id = factor(.data$item_id, levels = names(meta$item_names)),
+      score = as.integer(.data$score)
+    ) |>
+    dplyr::arrange(.data$resp_id, .data$item_id)
+
+  c(list(clean_data = clean_data), meta)
+}
+
+
+
+#' Title
+#'
+#' Item names can be provided in two places: As an identifier column in the
+#' Q-matrix, and as column names in the data. [reconcile_item_names()] ensures
+#' that the information from both sources (if provided) is consistent.
+#'
+#' @inheritParams abort_bad_argument
+#' @param data_names Item names provided as column names in the data.
+#' @param qmat_names Item names provided as identifiers in the Q-matrix.
+#' @param qmat_id The name of the identifier column in the Q-matrix.
+#' @param arg_data A character string with the name of the argument used to
+#'   provide the data.
+#' @param arg_qmat A character string with the name of the argument used to
+#'   provide the Q-matrix.
+#'
+#' @returns A list with two elements:
+#'  * `item_identifier`: The real name of the item identifier
+#'  * `item_names`: The real names of the items
+#' @noRd
+reconcile_item_names <- function(data_names, qmat_names, qmat_id,
+                                 arg_data, arg_qmat, call) {
+  extr_data <- setdiff(names(data_names), names(qmat_names))
+  miss_data <- setdiff(names(qmat_names), names(data_names))
+
+  if (length(data_names) != length(qmat_names)) {
+    abort_bad_argument(
+      arg = arg_data,
+      must = cli::format_message(c("have the same number of response columns",
+                                   "as there are items in the Q-matrix",
+                                   "specified by {.arg {arg_qmat}}")),
+      footer = c(
+        cli::format_message(
+          c(`!` = "Response columns found in: {length(data_names)}")
+        ),
+        cli::format_message(
+          c(`!` = "Items found in Q-matrix: {length(qmat_names)}")
+        )
+      ),
+      call = call
+    )
+  } else if (!is.null(qmat_id) && (length(extr_data) || length(miss_data))) {
+    abort_bad_argument(
+      arg = arg_data,
+      must = cli::format_message(c("must contain items that match those in the",
+                                   "Q-matrix specified by {.arg {arg_qmat}}")),
+      footer = c(
+        cli::format_message(
+          c(`!` = "Items found in {.arg {arg_data}} but not {.arg {arg_qmat}}:",
+            "{.val {cli::cli_vec(extr_data)}}")
+        ),
+        cli::format_message(
+          c(`!` = "Items found in {.arg {arg_qmat}} but not {.arg {arg_data}}:",
+            "{.val {cli::cli_vec(miss_data)}}")
+        )
+      ),
+      call = call
+    )
+  } else if (is.null(qmat_id)) {
+    qmat_id <- "item_id"
+    qmat_names <- rlang::set_names(qmat_names, names(data_names))
+  }
+
+  list(item_identifier = qmat_id,
+       item_names = qmat_names)
+}
---
+++ tests/testthat/test-check-data.R
@@ -0,0 +1,170 @@
+test_that("check data", {
+  err <- rlang::catch_cnd(check_data("a", identifier = NULL))
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "data frame")
+
+  test_d <- data.frame(resp = paste0("I", 1:5),
+                       item1 = sample(0:1, 5, replace = TRUE),
+                       item2 = sample(0:1, 5, replace = TRUE))
+  err <- rlang::catch_cnd(check_data(test_d, identifier = NULL))
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "only numeric values of 0 or 1")
+
+  test_d <- data.frame(resp = sample(0:1, 5, replace = TRUE),
+                       item2 = sample(1:2, 5, replace = TRUE),
+                       item3 = sample(2:3, 5, replace = TRUE))
+  err <- rlang::catch_cnd(check_data(test_d, "check1", identifier = NULL))
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "only 0 or 1")
+
+  test_d <- data.frame(resp = paste0("I", 1:5),
+                       item1 = sample(0:1, 5, replace = TRUE),
+                       item2 = sample(0:1, 5, replace = TRUE))
+  err <- rlang::catch_cnd(check_data(test_d, identifier = "resp_id"))
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "not found in")
+
+
+  test1_d <- tibble::tibble(resp = paste0("I_", 1:5),
+                            item1 = c(0, 1, 1, 0, 1),
+                            item2 = c(1, 0, 0, 1, 0),
+                            item3 = c(1, 1, 1, 0, 0))
+  test2_d <- tibble::tibble(resp = paste0("I_", 1:5),
+                            item1 = c(0L, 1L, 1L, 0L, 1L),
+                            item2 = c(1L, 0L, 0L, 1L, 0L),
+                            item3 = c(1L, 1L, 1L, 0L, 0L))
+  test3_d <- tibble::tibble(item1 = c(0L, 1L, 1L, 0L, 1L),
+                            item2 = c(1L, 0L, 0L, 1L, 0L),
+                            item3 = c(1L, 1L, 1L, 0L, 0L))
+  test4_d <- data.frame(resp = paste0("I_", 1:5),
+                        item1 = c(0, 1, 1, 0, 1),
+                        item2 = c(1, 0, 0, 1, 0),
+                        item3 = c(1, 1, 1, 0, 0))
+  test5_d <- data.frame(resp = paste0("I_", 1:5),
+                        item1 = c(0L, 1L, 1L, 0L, 1L),
+                        item2 = c(1L, 0L, 0L, 1L, 0L),
+                        item3 = c(1L, 1L, 1L, 0L, 0L))
+  test6_d <- data.frame(item1 = c(0L, 1L, 1L, 0L, 1L),
+                        item2 = c(1L, 0L, 0L, 1L, 0L),
+                        item3 = c(1L, 1L, 1L, 0L, 0L))
+  check_d <- tibble::tibble(resp = paste0("I_", 1:5),
+                            item1 = c(0L, 1L, 1L, 0L, 1L),
+                            item2 = c(1L, 0L, 0L, 1L, 0L),
+                            item3 = c(1L, 1L, 1L, 0L, 0L))
+  check_d_null <- tibble::tibble(item1 = c(0L, 1L, 1L, 0L, 1L),
+                                 item2 = c(1L, 0L, 0L, 1L, 0L),
+                                 item3 = c(1L, 1L, 1L, 0L, 0L))
+  expect_identical(check_data(test1_d, identifier = "resp"), check_d)
+  expect_identical(check_data(test2_d, identifier = "resp"), check_d)
+  expect_identical(check_data(test3_d, identifier = NULL), check_d_null)
+  expect_identical(check_data(test4_d, identifier = "resp"), check_d)
+  expect_identical(check_data(test5_d, identifier = "resp"), check_d)
+  expect_identical(check_data(test6_d, identifier = NULL), check_d_null)
+  expect_identical(check_data(check_d, identifier = "resp"), check_d)
+  expect_identical(check_data(check_d_null, identifier = NULL), check_d_null)
+})
+
+test_that("clean data", {
+  # dtmr -----------------------------------------------------------------------
+  cleaned_q <- clean_qmatrix(dcmdata::dtmr_qmatrix, identifier = "item")
+  cleaned_d <- clean_data(dcmdata::dtmr_data, identifier = "id",
+                          cleaned_qmatrix = cleaned_q)
+
+  expect_equal(length(cleaned_d), 5)
+  expect_equal(names(cleaned_d), c("clean_data", "item_identifier",
+                                   "item_names", "respondent_identifier",
+                                   "respondent_names"))
+
+  expect_equal(ncol(cleaned_d$clean_data), 3)
+  expect_equal(nrow(cleaned_d$clean_data),
+               nrow(dcmdata::dtmr_data) * (ncol(dcmdata::dtmr_data) - 1))
+  expect_equal(colnames(cleaned_d$clean_data), c("resp_id", "item_id", "score"))
+  expect_equal(vapply(cleaned_d$clean_data, typeof, character(1)),
+               c(resp_id = "integer", item_id = "integer", score = "integer"))
+  expect_equal(vapply(cleaned_d$clean_data, is.factor, logical(1)),
+               c(resp_id = TRUE, item_id = TRUE, score = FALSE))
+
+  expect_equal(cleaned_d$item_identifier, colnames(dcmdata::dtmr_qmatrix)[1])
+  expect_equal(names(cleaned_d$item_names), dcmdata::dtmr_qmatrix$item)
+  expect_equal(cleaned_d$respondent_identifier, colnames(dcmdata::dtmr_data)[1])
+  expect_equal(names(cleaned_d$respondent_names),
+               as.character(dcmdata::dtmr_data$id))
+
+  # ecpe -----------------------------------------------------------------------
+  cleaned_q <- clean_qmatrix(dcmdata::ecpe_qmatrix, identifier = "item_id")
+  cleaned_d <- clean_data(dcmdata::ecpe_data[, -1],
+                          cleaned_qmatrix = cleaned_q)
+
+  expect_equal(length(cleaned_d), 5)
+  expect_equal(names(cleaned_d), c("clean_data", "item_identifier",
+                                   "item_names", "respondent_identifier",
+                                   "respondent_names"))
+
+  expect_equal(ncol(cleaned_d$clean_data), 3)
+  expect_equal(nrow(cleaned_d$clean_data),
+               nrow(dcmdata::ecpe_data) * (ncol(dcmdata::ecpe_data) - 1))
+  expect_equal(colnames(cleaned_d$clean_data), c("resp_id", "item_id", "score"))
+  expect_equal(vapply(cleaned_d$clean_data, typeof, character(1)),
+               c(resp_id = "integer", item_id = "integer", score = "integer"))
+  expect_equal(vapply(cleaned_d$clean_data, is.factor, logical(1)),
+               c(resp_id = TRUE, item_id = TRUE, score = FALSE))
+
+  expect_equal(cleaned_d$item_identifier, colnames(dcmdata::ecpe_qmatrix)[1])
+  expect_equal(names(cleaned_d$item_names), dcmdata::ecpe_qmatrix$item_id)
+  expect_equal(cleaned_d$respondent_identifier, "resp_id")
+  expect_equal(names(cleaned_d$respondent_names),
+               as.character(seq_len(nrow(dcmdata::ecpe_data))))
+
+  # mdm ------------------------------------------------------------------------
+  cleaned_q <- clean_qmatrix(dcmdata::mdm_qmatrix[, -1])
+  cleaned_d <- clean_data(dcmdata::mdm_data, identifier = "respondent",
+                          cleaned_qmatrix = cleaned_q)
+
+  expect_equal(length(cleaned_d), 5)
+  expect_equal(names(cleaned_d), c("clean_data", "item_identifier",
+                                   "item_names", "respondent_identifier",
+                                   "respondent_names"))
+
+  expect_equal(ncol(cleaned_d$clean_data), 3)
+  expect_equal(nrow(cleaned_d$clean_data),
+               nrow(dcmdata::mdm_data) * (ncol(dcmdata::mdm_data) - 1))
+  expect_equal(colnames(cleaned_d$clean_data), c("resp_id", "item_id", "score"))
+  expect_equal(vapply(cleaned_d$clean_data, typeof, character(1)),
+               c(resp_id = "integer", item_id = "integer", score = "integer"))
+  expect_equal(vapply(cleaned_d$clean_data, is.factor, logical(1)),
+               c(resp_id = TRUE, item_id = TRUE, score = FALSE))
+
+  expect_equal(cleaned_d$item_identifier, "item_id")
+  expect_equal(names(cleaned_d$item_names), colnames(dcmdata::mdm_data[, -1]))
+  expect_equal(cleaned_d$respondent_identifier, colnames(dcmdata::mdm_data)[1])
+  expect_equal(names(cleaned_d$respondent_names),
+               as.character(dcmdata::mdm_data$respondent))
+})
+
+test_that("reconcile item names", {
+  # different numbers of items -------------------------------------------------
+  cleaned_q <- clean_qmatrix(dcmdata::dtmr_qmatrix, identifier = "item")
+  my_dat <- dcmdata::mdm_data
+
+  err <- rlang::catch_cnd(
+    clean_data(x = my_dat, identifier = "respondent",
+               cleaned_qmatrix = cleaned_q)
+  )
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "same number of response columns")
+
+  # name mismatch --------------------------------------------------------------
+  cleaned_q <- clean_qmatrix(dcmdata::ecpe_qmatrix, identifier = "item_id")
+  my_dat <- dcmdata::ecpe_data |>
+    dplyr::rename(J1 = "E1", J13 = "E13")
+
+  err <- rlang::catch_cnd(
+    clean_data(x = my_dat, identifier = "resp_id", cleaned_qmatrix = cleaned_q)
+  )
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "contain items that match those in the Q-matrix")
+  expect_match(err$footer[1], paste0("Items found in `my_dat` but not ",
+                                     "`cleaned_q`:.*J1.* and .*J13.*"))
+  expect_match(err$footer[2], paste0("Items found in `cleaned_q` but not ",
+                                     "`my_dat`:.*E1.* and .*E13.*"))
+})
